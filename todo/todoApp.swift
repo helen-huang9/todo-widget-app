879812d6609875d4c02cfce62bf71d898140e65e
Helen Huang <helen_huang@brown.edu>
2022-11-16 13:01:43 -0500

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Helen Huang on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
